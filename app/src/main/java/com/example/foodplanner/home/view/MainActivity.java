5ebf210999771fc18b4d0dc15baf3f767de79033
fatma=Elzeny <fatmayosry9@gmail.com>
2025-04-29 13:12:30 +0300

    Structure of How to handle the navigation Bar

--- app/src/main/java/com/example/foodplanner/home/view/MainActivity.java
+++ app/src/main/java/com/example/foodplanner/home/view/MainActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.foodplanner.FavoritesActivity;
 import com.example.foodplanner.MealDetail.view.MealDetailsActivity;
+import com.example.foodplanner.PlannerActivity;
+import com.example.foodplanner.ProfileActivity;
 import com.example.foodplanner.R;
 
 import android.content.Intent;
@@ -14,11 +17,13 @@ import android.widget.*;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
+import com.example.foodplanner.SearchActivity;
 import com.example.foodplanner.home.presenter.HomePresenter;
 import com.example.foodplanner.home.presenter.HomePresenterImpl;
 import com.example.foodplanner.model.Meal;
 import com.example.foodplanner.model.MealsRepository;
 import com.example.foodplanner.model.MealsRepositoryImpl;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.List;
 
@@ -53,6 +58,29 @@ public class MainActivity extends AppCompatActivity implements HomeView,OnMealCl
 
         presenter.getMealOfTheDay();
         presenter.getMealsByCategory("Beef");
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.navigation_home) {
+                return true; // Stay here
+            } else if (id == R.id.navigation_favorites) {
+                startActivity(new Intent(this, FavoritesActivity.class));
+                return true;
+            } else if (id == R.id.navigation_planner) {
+                startActivity(new Intent(this, PlannerActivity.class));
+                return true;
+            } else if (id == R.id.navigation_profile) {
+                startActivity(new Intent(this, ProfileActivity.class));
+                return true;
+            } else if (id == R.id.navigation_search) {
+                startActivity(new Intent(this, SearchActivity.class));
+                return true;
+            }
+
+            return false;
+        });
 
 
     }
